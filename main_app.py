bb49cdd11adcdfdfcb344afe52d660b032e3a2c6
ruderikissa <36214630+ruderikissa@users.noreply.github.com>
2023-08-09 16:58:37 -0600

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -1,10 +1,8 @@
 from flask import Flask
 from flask import jsonify
-
 app = Flask(__name__)
 
 @app.route('/')
-
 def hello():
     """Return a friendly HTTP greeting."""
     print("I am inside hello world")
@@ -15,6 +13,7 @@ def echo(name):
     print(f"This was placed in the url: new-{name}")
     val = {"new-name": name}
     return jsonify(val)
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
